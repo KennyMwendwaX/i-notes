3da66458e4bb7c4eef79a2644f29653a8db913d1
IconicDexter <kennymwendwa67@gmail.com>
2023-05-21 13:11:05 +0300

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
+import Note from "@/components/Note";
+import {} from "react-icons/";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="body-font text-gray-600">
+        <div className="container mx-auto px-5 py-24">
+          <div className="-m-4 flex flex-wrap">
+            <div className="p-4 lg:w-1/4">
+              <Note />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
