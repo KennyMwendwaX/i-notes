f57cc0a22acec9cba6c768fe82d83f38a1cdfe12
IconicDexter <kennymwendwa67@gmail.com>
2023-06-01 18:01:41 +0300

    commit

--- utils/DateTime.ts
+++ utils/DateTime.ts
@@ -2,12 +2,11 @@ import format from "date-fns/format";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 export function formatDateTime(timestamp: number): string {
-  const date = new Date(timestamp);
-  return format(date, "dd MMM yyyy, HH:mm");
+  const noteDate = new Date(timestamp);
+  return format(noteDate, "dd MMM yyyy, HH:mm");
 }
 
 export function getTimeAgo(timestamp: number): string {
-  const currentDate = new Date();
   const noteDate = new Date(timestamp);
   return formatDistanceToNow(noteDate, { addSuffix: true });
 }
