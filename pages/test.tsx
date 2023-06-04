0b59cd479ea1335e69af8b975216da0c90e949f2
IconicDexter <kennymwendwa67@gmail.com>
2023-06-04 17:36:24 +0300

    Toolbar cleanup

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,29 +1,17 @@
-import {
-  $createParagraphNode,
-  $createTextNode,
-  $getRoot,
-  $getSelection,
-  $isRangeSelection,
-  EditorState,
-  ParagraphNode,
-} from "lexical";
+import { EditorState, ParagraphNode } from "lexical";
 
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { ListPlugin } from "@lexical/react/LexicalListPlugin";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
-import { HeadingNode, $createHeadingNode } from "@lexical/rich-text";
-import { $setBlocksType } from "@lexical/selection";
-import {
-  INSERT_ORDERED_LIST_COMMAND,
-  INSERT_UNORDERED_LIST_COMMAND,
-  ListNode,
-  ListItemNode,
-} from "@lexical/list";
+import { HeadingNode } from "@lexical/rich-text";
+import { ListNode, ListItemNode } from "@lexical/list";
 import { useEffect } from "react";
+import ToolbarPlugin from "@/components/Toolbar";
 
 const theme = {
   // Theme styling goes here
@@ -66,70 +54,6 @@ function onError(error: Error) {
   console.log(error);
 }
 
-type HeadingTagType = "h1" | "h2" | "h3";
-
-function HeadingToolbarPlugin(): JSX.Element {
-  const [editor] = useLexicalComposerContext();
-  const HeadingTags = ["h1", "h2", "h3"];
-  const onClick = (tag: HeadingTagType): void => {
-    editor.update(() => {
-      const selection = $getSelection();
-      if ($isRangeSelection(selection)) {
-        $setBlocksType(selection, () => $createHeadingNode(tag));
-      }
-    });
-  };
-
-  return (
-    <div className="space-x-2">
-      {HeadingTags.map((tag) => (
-        <button
-          className="rounded-lg bg-blue-600 px-3 py-2 text-white"
-          onClick={() => onClick(tag as HeadingTagType)}
-          key={tag}>
-          {tag.toUpperCase()}
-        </button>
-      ))}
-    </div>
-  );
-}
-
-type ListTagType = "ol" | "ul";
-
-function ListToolbarPlugin(): JSX.Element {
-  const [editor] = useLexicalComposerContext();
-  const listTags = ["ol", "ul"];
-  const onClick = (tag: ListTagType): void => {
-    if (tag === "ol") {
-      editor.dispatchCommand(INSERT_ORDERED_LIST_COMMAND, undefined);
-      return;
-    }
-    editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND, undefined);
-  };
-
-  return (
-    <div className="space-x-2">
-      {listTags.map((tag) => (
-        <button
-          className="rounded-lg bg-blue-600 px-3 py-2 text-white"
-          onClick={() => onClick(tag as ListTagType)}
-          key={tag}>
-          {tag.toUpperCase()}
-        </button>
-      ))}
-    </div>
-  );
-}
-
-function ToolbarPlugin(): JSX.Element {
-  return (
-    <div className="flex items-center space-x-2">
-      <HeadingToolbarPlugin />
-      <ListToolbarPlugin />
-    </div>
-  );
-}
-
 export default function Test() {
   const initialConfig = {
     namespace: "MyEditor",
@@ -142,6 +66,7 @@ export default function Test() {
       <div className="container mx-auto mb-2 px-5 py-20">
         <LexicalComposer initialConfig={initialConfig}>
           <ToolbarPlugin />
+          <ListPlugin />
           <RichTextPlugin
             contentEditable={
               <ContentEditable className="block h-96 w-3/5 rounded-lg border-0 bg-white p-3 text-sm text-gray-800 outline-none focus:ring-0 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400" />
